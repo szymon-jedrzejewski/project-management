da61bae76c9a95dd23f8fe74ff4713f9e2c7e364
Szymon Jedrzejewski <Szymon.Jedrzejewski@ttpsc.pl>
2020-11-04 11:32:18 +0100

    Add new fields to 'Projects'

--- src/main/java/pl/ttpsc/javaupdate/project/model/Project.java
+++ src/main/java/pl/ttpsc/javaupdate/project/model/Project.java
@@ -2,9 +2,14 @@ package pl.ttpsc.javaupdate.project.model;
 
 import pl.ttpsc.javaupdate.project.persistence.Persistable;
 
+import java.util.List;
+
 public class Project implements Persistable {
     private String name;
     private String description;
+    private String creator;
+    private List<Document> documents;
+    private List<User> assignedUsers;
 
     public String getName() {
         return name;
@@ -22,11 +27,38 @@ public class Project implements Persistable {
         this.description = description;
     }
 
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public List<Document> getDocuments() {
+        return documents;
+    }
+
+    public void setDocuments(List<Document> documents) {
+        this.documents = documents;
+    }
+
+    public List<User> getAssignedUsers() {
+        return assignedUsers;
+    }
+
+    public void setAssignedUsers(List<User> assignedUsers) {
+        this.assignedUsers = assignedUsers;
+    }
+
     @Override
     public String toString() {
         return "Project{" +
                 "name='" + name + '\'' +
                 ", description='" + description + '\'' +
+                ", creator='" + creator + '\'' +
+                ", documents=" + documents +
+                ", assignedUsers=" + assignedUsers +
                 '}';
     }
 }
