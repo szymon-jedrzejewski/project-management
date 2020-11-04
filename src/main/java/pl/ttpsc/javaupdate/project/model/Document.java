659d9ae812d7f9099a2b08753c7d759afe023e52
Szymon Jedrzejewski <Szymon.Jedrzejewski@ttpsc.pl>
2020-11-04 12:15:28 +0100

    Add necessary fields to models

--- src/main/java/pl/ttpsc/javaupdate/project/model/Document.java
+++ src/main/java/pl/ttpsc/javaupdate/project/model/Document.java
@@ -1,4 +1,72 @@
 package pl.ttpsc.javaupdate.project.model;
 
 public class Document {
+
+    private String title;
+    private String description;
+    private User creator;
+    private String topic;
+    private String content;
+
+    public Document() {
+    }
+
+    public Document(String title, String description, User creator, String topic, String content) {
+        this.title = title;
+        this.description = description;
+        this.creator = creator;
+        this.topic = topic;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Document{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", creator=" + creator +
+                ", topic='" + topic + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
--- src/main/java/pl/ttpsc/javaupdate/project/model/User.java
+++ src/main/java/pl/ttpsc/javaupdate/project/model/User.java
@@ -1,4 +1,71 @@
 package pl.ttpsc.javaupdate.project.model;
 
 public class User {
+    private String firstname;
+    private String lastName;
+    private String username;
+    private String password;
+    private Role systemRole;
+
+    public User() {
+    }
+
+    public User(String firstname, String lastName, String username, String password, Role systemRole) {
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.systemRole = systemRole;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getSystemRole() {
+        return systemRole;
+    }
+
+    public void setSystemRole(Role systemRole) {
+        this.systemRole = systemRole;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstname='" + firstname + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", systemRole=" + systemRole +
+                '}';
+    }
 }
