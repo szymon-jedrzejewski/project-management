0981f78b35429268aecb77cdaec88783cfcb1646
Szymon Jedrzejewski <Szymon.Jedrzejewski@ttpsc.pl>
2020-11-06 08:26:15 +0100

    Move methods responsible for generating queries to 'SqlQueryUtility'

--- src/main/java/pl/ttpsc/javaupdate/project/persistence/sql/SqlPersistenceManager.java
+++ src/main/java/pl/ttpsc/javaupdate/project/persistence/sql/SqlPersistenceManager.java
@@ -5,8 +5,8 @@ import org.apache.logging.log4j.Logger;
 import pl.ttpsc.javaupdate.project.persistence.Persistable;
 import pl.ttpsc.javaupdate.project.persistence.PersistenceManager;
 import pl.ttpsc.javaupdate.project.persistence.QuerySpec;
+import pl.ttpsc.javaupdate.project.utility.SqlQueryUtility;
 
-import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -26,9 +26,8 @@ public class SqlPersistenceManager implements PersistenceManager {
 
     @Override
     public Persistable create(Persistable persistable) {
-        String query = getFullQuery(persistable);
-
-        logger.info("Generated query: " + query);
+        String query = SqlQueryUtility.getFullQuery(persistable);
+        logger.debug("Generated query: " + query);
 
         try {
             Statement statement = connection.createStatement();
@@ -39,13 +38,6 @@ public class SqlPersistenceManager implements PersistenceManager {
         }
 
         return null;
-    }
-
-    private String getFullQuery(Persistable persistable) {
-        return  "INSERT INTO "
-                + persistable.getClass().getSimpleName().toLowerCase() + "s "
-                + "(" + getFieldsNames(persistable) + ") "
-                + "VALUES (" + getFieldsValues(persistable) + ");";
     }
 
     @Override
@@ -61,53 +53,5 @@ public class SqlPersistenceManager implements PersistenceManager {
     @Override
     public List<Persistable> find(QuerySpec querySpec) {
         return null;
-    }
-
-    private String getFieldsNames(Persistable persistable) {
-        Field[] fields =  persistable.getClass().getDeclaredFields();
-        StringBuilder query = new StringBuilder();
-
-        for (Field field : fields) {
-            field.setAccessible(true);
-            query.append(field.getName());
-            query.append(", ");
-            logger.info("Actual query: " + query);
-        }
-
-        return query.deleteCharAt(query.lastIndexOf(",")).toString().trim();
-    }
-
-    private String getFieldsValues(Persistable persistable) {
-        Field[] fields =  persistable.getClass().getDeclaredFields();
-        StringBuilder query = new StringBuilder();
-
-        for(Field field : fields) {
-            field.setAccessible(true);
-            logger.info("Field type: " + field.getType());
-            try {
-                Object object = field.get(persistable);
-                logger.info("Field value: " + object);
-                logger.info("Integer: " + field.getType().isAssignableFrom(Integer.class));
-                if(field.getType().isAssignableFrom(String.class)) {
-                    query.append("'")
-                            .append(object)
-                            .append("'")
-                            .append(", ");
-                } else if(field.getType().isAssignableFrom(Boolean.class)) {
-                    query.append(String.valueOf(object).toUpperCase())
-                            .append(", ");
-                } else {
-                    query.append(object)
-                            .append(", ");
-                }
-
-            } catch (IllegalAccessException e) {
-                logger.error("Field not found");
-                logger.error(e.getMessage());
-            }
-        }
-
-        logger.info("Fields values: " + query);
-        return query.deleteCharAt(query.lastIndexOf(",")).toString().trim();
     }
 }
---
+++ src/main/java/pl/ttpsc/javaupdate/project/utility/SqlQueryUtility.java
@@ -0,0 +1,74 @@
+package pl.ttpsc.javaupdate.project.utility;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.ttpsc.javaupdate.project.persistence.Persistable;
+import pl.ttpsc.javaupdate.project.persistence.sql.SqlPersistenceManager;
+
+import java.lang.reflect.Field;
+
+public final class SqlQueryUtility {
+    private static final Logger logger = LogManager.getLogger(SqlPersistenceManager.class);
+
+    private SqlQueryUtility() {
+    }
+
+    public static String generateTableName(Persistable persistable) {
+        return persistable.getClass().getSimpleName().toLowerCase() + "s ";
+    }
+
+    public static String getFullQuery(Persistable persistable) {
+        return "INSERT INTO "
+                + SqlQueryUtility.generateTableName(persistable)
+                + "(" + SqlQueryUtility.generateFieldsNames(persistable) + ") "
+                + "VALUES (" + SqlQueryUtility.generateFieldsValues(persistable) + ");";
+    }
+
+    public static String generateFieldsNames(Persistable persistable) {
+        Field[] fields =  persistable.getClass().getDeclaredFields();
+        StringBuilder query = new StringBuilder();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            query.append(field.getName());
+            query.append(", ");
+            logger.debug("Actual query: " + query);
+        }
+
+        return query.deleteCharAt(query.lastIndexOf(",")).toString().trim();
+    }
+
+    public static String generateFieldsValues(Persistable persistable) {
+        Field[] fields =  persistable.getClass().getDeclaredFields();
+        StringBuilder query = new StringBuilder();
+
+        for(Field field : fields) {
+            field.setAccessible(true);
+            logger.debug("Field type: " + field.getType());
+            try {
+                Object object = field.get(persistable);
+                logger.debug("Field value: " + object);
+                logger.debug("Integer: " + field.getType().isAssignableFrom(Integer.class));
+                if(field.getType().isAssignableFrom(String.class)) {
+                    query.append("'")
+                            .append(object)
+                            .append("'")
+                            .append(", ");
+                } else if(field.getType().isAssignableFrom(Boolean.class)) {
+                    query.append(String.valueOf(object).toUpperCase())
+                            .append(", ");
+                } else {
+                    query.append(object)
+                            .append(", ");
+                }
+
+            } catch (IllegalAccessException e) {
+                logger.error("Field not found");
+                logger.error(e.getMessage());
+            }
+        }
+
+        logger.debug("Fields values: " + query);
+        return query.deleteCharAt(query.lastIndexOf(",")).toString().trim();
+    }
+}
