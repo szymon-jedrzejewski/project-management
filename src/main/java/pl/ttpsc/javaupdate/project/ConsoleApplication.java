95a841691ad7c5ac83bbb8bc30b400eb601303e5
Szymon Jedrzejewski <Szymon.Jedrzejewski@ttpsc.pl>
2020-11-09 11:57:16 +0100

    Remove unnecessary imports

--- src/main/java/pl/ttpsc/javaupdate/project/ConsoleApplication.java
+++ src/main/java/pl/ttpsc/javaupdate/project/ConsoleApplication.java
@@ -3,27 +3,20 @@ package pl.ttpsc.javaupdate.project;
 import pl.ttpsc.javaupdate.project.action.Action;
 import pl.ttpsc.javaupdate.project.action.ShowProjectsAction;
 import pl.ttpsc.javaupdate.project.config.Config;
-import pl.ttpsc.javaupdate.project.model.Project;
-import pl.ttpsc.javaupdate.project.persistence.Persistable;
-import pl.ttpsc.javaupdate.project.persistence.QuerySpec;
 import pl.ttpsc.javaupdate.project.persistence.sql.SqlPersistenceManager;
 import pl.ttpsc.javaupdate.project.repository.ProjectRepository;
 import pl.ttpsc.javaupdate.project.view.console.ConsoleMenu;
 import pl.ttpsc.javaupdate.project.view.console.ProjectConsoleView;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class ConsoleApplication {
 
-    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
+    public static void main(String[] args) {
         Config config = new Config()
                 .withSqlPersistence();
 
