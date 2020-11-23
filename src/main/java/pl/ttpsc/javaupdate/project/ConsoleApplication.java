e6f696cee481940eefae8400781a55d43ff3633f
Szymon Jedrzejewski <szymonj219@gmail.com>
2020-11-23 21:36:52 +0100

    Move database credentials to

--- src/main/java/pl/ttpsc/javaupdate/project/ConsoleApplication.java
+++ src/main/java/pl/ttpsc/javaupdate/project/ConsoleApplication.java
@@ -2,6 +2,9 @@ package pl.ttpsc.javaupdate.project;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import pl.ttpsc.javaupdate.project.action.*;
 import pl.ttpsc.javaupdate.project.action.project.*;
 import pl.ttpsc.javaupdate.project.config.Config;
@@ -10,6 +13,10 @@ import pl.ttpsc.javaupdate.project.repository.ProjectRepository;
 import pl.ttpsc.javaupdate.project.view.console.ConsoleMenu;
 import pl.ttpsc.javaupdate.project.view.console.ProjectConsoleView;
 
+import javax.json.stream.JsonParser;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -21,31 +28,26 @@ public class ConsoleApplication {
     private static final Logger logger = LogManager.getLogger(SqlPersistenceManager.class);
 
     public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, NoSuchFieldException {
-        Config config = new Config()
-                .withSqlPersistence();
-
-        ConsoleMenu menu = new ConsoleMenu();
-
-        Connection connection;
-        String url = "jdbc:postgresql://localhost:5432/project_management";
-
-        String login = "postgres";
-        String password = "postgres";
-
 
         try {
-            connection = DriverManager.getConnection(url, login, password);
+            Config config = new Config()
+                    .withSqlPersistence();
+            Connection connection = DriverManager.getConnection(config.getUlr(), config.getLogin(), config.getPassword());
+
             List<Action> actions = new ArrayList<>();
+
             actions.add(new ShowAllProjectsAction(new ProjectConsoleView(), new ProjectRepository(new SqlPersistenceManager(connection))));
             actions.add(new ShowProjectAction(new ProjectConsoleView(), new ProjectRepository(new SqlPersistenceManager(connection))));
             actions.add(new CreateProjectAction(new ProjectConsoleView(), new ProjectRepository(new SqlPersistenceManager(connection))));
             actions.add(new UpdateProjectAction(new ProjectConsoleView(), new ProjectRepository(new SqlPersistenceManager(connection))));
             actions.add(new DeleteProjectAction(new ProjectConsoleView(), new ProjectRepository(new SqlPersistenceManager(connection))));
             actions.add(new ExitAction(connection, new ProjectConsoleView()));
+
+            ConsoleMenu menu = new ConsoleMenu();
             Action action = menu.choseAction(actions);
             action.execute();
         } catch (SQLException e) {
-            logger.error("SQLException: " + e.getMessage());
+            logger.error("Exception: " + e.getMessage());
             System.out.println("Application start failed.");
         }
     }
--- src/main/java/pl/ttpsc/javaupdate/project/config/Config.java
+++ src/main/java/pl/ttpsc/javaupdate/project/config/Config.java
@@ -1,14 +1,33 @@
 package pl.ttpsc.javaupdate.project.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import pl.ttpsc.javaupdate.project.action.Action;
 import pl.ttpsc.javaupdate.project.persistence.PersistenceManager;
 import pl.ttpsc.javaupdate.project.persistence.sql.SqlPersistenceManager;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 public class Config {
+    private static final Logger logger = LogManager.getLogger(SqlPersistenceManager.class);
     private PersistenceManager persistenceManager;
     private List<Action> actions;
+    private JSONParser parser;
+    private JSONObject object;
+
+    public Config() {
+        parser = new JSONParser();
+        try {
+            object  = (JSONObject)parser.parse(new FileReader("src/main/resources/databaseCredentials.json"));
+        } catch (IOException | ParseException e) {
+            logger.error("Exception: " + e.getMessage());
+        }
+    }
 
     public PersistenceManager getPersistenceManager() {
         return persistenceManager;
@@ -29,6 +48,18 @@ public class Config {
     public Config withSqlPersistence() {
         setPersistenceManager(new SqlPersistenceManager());
         return this;
+    }
+
+    public String getUlr() {
+        return (String)object.get("url");
+    }
+
+    public String getLogin() {
+        return (String)object.get("login");
+    }
+
+    public String getPassword() {
+        return (String)object.get("password");
     }
 
     public Config withXmlPersistence() {
